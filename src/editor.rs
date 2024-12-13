3c89cc60d39a475f172bb2bf5ce29bb50316c301
Doruk <doruk.cem@gmail.com>
2024-12-13 13:51:42 +0300

    refactor to use crossterm

--- src/editor.rs
+++ src/editor.rs
@@ -1,32 +1,35 @@
-use std::io::{self, Read};
+use crossterm::{
+    event::{Event, KeyCode::Char},
+    terminal,
+};
 
-use crossterm::terminal;
-
-pub struct Editor{
-
-}
+pub struct Editor {}
 
 impl Editor {
     pub fn default() -> Self {
-        Editor {  }
+        Editor {}
     }
 
-    pub fn run(&self){
+    pub fn run(&self) {
         terminal::enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            let b = b.unwrap();
-            let c = b as char;
-    
-            if c.is_control() {
-                println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-            } else {
-                println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-            }
-    
-            if c == 'q' {
-                break;
+        loop {
+            match crossterm::event::read() {
+                Ok(Event::Key(event)) => {
+                    println!("{:?} \r", event);
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        }
+                        _ => (),
+                    }
+                }
+                Err(err) => println!("{err}"),
+                _ => (),
             }
         }
+
         terminal::disable_raw_mode().unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 use editor::Editor;
 
 mod editor;
 fn main() {
-    let editor   =  Editor::default();
-    editor.run()
+    let editor = Editor::default();
+    editor.run();
 }
